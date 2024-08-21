9bcfc8743b6ee23026b15a3619a69569490f0ac5
Piotr Krupa <piotrkrupa06@gmail.com>
2024-08-21 21:20:18 +0200

    Rework session discovery

--- Flow.Launcher.Plugin.FlowTrumpet/Audio/AudioSessionManager.cs
+++ Flow.Launcher.Plugin.FlowTrumpet/Audio/AudioSessionManager.cs
@@ -1,6 +1,7 @@
 ﻿using CoreAudio;
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml.Linq;
@@ -14,10 +15,14 @@ namespace Flow.Launcher.Plugin.FlowTrumpet.Audio
         private MMDeviceEnumerator _deviceEnumerator;
         private MMNotificationClient _notfClient;
 
+        private List<AudioSessionInfo> _sessions = new();
+
         public AudioSessionManager()
         {
             _deviceEnumerator = new MMDeviceEnumerator(Guid.NewGuid());
-            _deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            _activeDevice = _deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            _activeDevice.AudioSessionManager2.OnSessionCreated += HandleSessionCreated;
 
             _notfClient = new MMNotificationClient(_deviceEnumerator);
 
@@ -26,34 +31,42 @@ namespace Flow.Launcher.Plugin.FlowTrumpet.Audio
                 _activeDevice = _deviceEnumerator.GetDevice(e.DeviceId);
             };
 
-            foreach (var dev in _deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
-            {
-                if (dev.Selected)
-                {
-                    _activeDevice = dev;
-                    break;
-                }
-            }
-
             if (_activeDevice == null)
             {
                 throw new Exception("Device not found.");
             }
+
+            foreach (var session in _activeDevice.AudioSessionManager2.Sessions)
+            {
+                _sessions.Add(CreateSessionInfo(session));
+            }
+        }
+
+        private void HandleSessionCreated(object sender, CoreAudio.Interfaces.IAudioSessionControl2 newSession)
+        {
+            AudioSessionManager2 asm = (AudioSessionManager2)sender;
+
+            newSession.GetProcessId(out uint newSessionId);
+
+            asm.RefreshSessions();
+            var session = asm.Sessions.FirstOrDefault(x => x.ProcessID == newSessionId);
+
+            if (session == null)
+            {
+                return;
+            }
+
+            _sessions.Add(CreateSessionInfo(session));
         }
 
         public AudioSessionInfo GetSessionInfo(uint processId)
         {
-            var session = _activeDevice.AudioSessionManager2.Sessions.FirstOrDefault(x => x.ProcessID == processId);
-
-            return CreateSessionInfo(session);
+            return _sessions.FirstOrDefault(x => x.ProcessId == processId);
         }
 
         public IEnumerable<AudioSessionInfo> GetSessionsInfo()
         {
-            foreach (var session in _activeDevice.AudioSessionManager2.Sessions)
-            {
-                yield return CreateSessionInfo(session);
-            }
+            return _sessions;
         }
 
         public void SetSessionVolume(uint processId, float newVolume)
@@ -70,6 +83,18 @@ namespace Flow.Launcher.Plugin.FlowTrumpet.Audio
 
         private AudioSessionInfo CreateSessionInfo(AudioSessionControl2 session)
         {
+            if (session == null)
+            {
+                return null;
+            }
+
+            if (session.State == AudioSessionState.AudioSessionStateExpired)
+            {
+                return null;
+            }
+
+            session.OnStateChanged += Session_OnStateChanged;
+
             Process p = Process.GetProcessById((int)session.ProcessID);
             var name = session.IsSystemSoundsSession ? "System Sounds" : session.DisplayName;
             if (name == "")
@@ -84,6 +109,23 @@ namespace Flow.Launcher.Plugin.FlowTrumpet.Audio
                 Volume = session.SimpleAudioVolume.MasterVolume,
                 IcoPath = ProcessHelper.TryGetProcessIconFilename(p),
             };
+        }
+
+        private void Session_OnStateChanged(object sender, AudioSessionState newState)
+        {
+            if (newState == AudioSessionState.AudioSessionStateExpired)
+            {
+                AudioSessionControl2 session = (AudioSessionControl2)sender;
+
+                var sessionInfo = _sessions.FirstOrDefault(x => x.ProcessId == session.ProcessID);
+
+                if (sessionInfo == null)
+                {
+                    return;
+                }
+
+                _sessions.Remove(sessionInfo);
+            }
         }
     }
 }
